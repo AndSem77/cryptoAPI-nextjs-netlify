bc7aa99be37188da3b0756753b38b1d97bca2916
Andrey Semeyko <andsemua@gmail.com>
2021-02-03 14:38:02 +0400

    updated text

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,22 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className='container'>
       <Head>
         <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title='Welcome to my app!' />
+        <h1>I love React JS</h1>
+        <p>I am now live on Netlify</p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
